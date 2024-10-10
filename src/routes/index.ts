ce1cc001537b9ff75f5d29d222ad0b683520e869
Mahfuz <aamahfuz.pro@gmail.com>
2024-10-10 19:10:47 +0600

    basic setup done

---
+++ src/routes/index.ts
@@ -0,0 +1,42 @@
+
+import express from 'express';
+import { AuthRoutes } from '../modules/auth/auth.route';
+import { CommentRoutes } from '../modules/comments/comment.route';
+import { UserRoutes } from '../modules/user/user.route';
+import { PaymentRoutes } from '../modules/payment/payment.route';
+import { PostRoutes } from '../modules/posts/post.route';
+import { StatisticsRoutes } from '../modules/statistics/statistics.route';
+
+const router = express.Router()
+
+
+const moduleRoutes = [
+    {
+      path: '/auth',
+      route: AuthRoutes,
+    },
+    {
+      path: '/users',
+      route: UserRoutes,
+    },
+    {
+      path: '/posts',
+      route: PostRoutes,
+    },
+    {
+      path: '/comments',
+      route: CommentRoutes,
+    },
+    {
+      path: '/payments',
+      route: PaymentRoutes,
+    },
+    {
+      path: '/statistics',
+      route: StatisticsRoutes,
+    },
+  ];
+  
+  moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
