79c59ecc3aa73b4c488b6a0951ff3dcd0377fdd2
Mahfuz <aamahfuz.pro@gmail.com>
2024-11-10 17:44:25 +0600

    some changed

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,51 @@
-import express from 'express'
-import router from './routes'
-import globalErrorHandler from './middlewares/globalErrorHandler'
-import cors from 'cors'
+import express from 'express';
+import router from './routes';
+import globalErrorHandler from './middlewares/globalErrorHandler';
+import cors from 'cors';
 
-const app = express()
+const app = express();
 
-// use json body parser 
-app.use(express.json())
+// Define allowed origins
+const allowedOrigins = [
+  'https://tricks-hub-client.vercel.app',
+  "https://tricks-hub-server.vercel.app",
+  'http://localhost:3000', 
+];
 
-// use cors 
-app.use(cors( {origin: "*"}));
-app.use(cors( {origin: "http://localhost:3000"}));
+// Set up CORS to check against allowed origins
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin, like mobile apps or CURL requests
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error('Not allowed by CORS'));
+      }
+    },
+  })
+);
 
-// use router
-app.use('/api', router)
+// Use JSON body parser
+app.use(express.json());
 
-
-
+// Use router
+app.use('/api', router);
 
 app.get('/', (req, res) => {
-  res.send('Welcome to tricks hub server!😄💖')
-})
+  res.send('Welcome to tricks hub server!😄💖');
+});
 
-// global error handler 
-app.use(globalErrorHandler)
-
+// Global error handler
+app.use(globalErrorHandler);
 
 app.all('*', (req, res) => {
   res.status(404).json({
-    "success" : false,
-    "statuscode" : 404,
-    "message" : "Not found"
-  })
-})
+    success: false,
+    statuscode: 404,
+    message: 'Not found',
+  });
+});
 
-export default app
+export default app;
